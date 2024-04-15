a836b5c503bdcfcd8017f35d3b833c7926b1c62d
SHAIK SAMEER BASHA <118707756+shaikSameerbasha5404@users.noreply.github.com>
2024-04-15 21:39:08 +0530

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,34 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10f;
-    public GameObject projectilePrefab; // for reusing the object
-    // Start is called before the first frame update
+    public GameObject projectilePrefab;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xRange)
+        if(transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xRange)
+        if(transform.position.x>xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab,transform.position,projectilePrefab.transform.rotation);
         }
     }
-
 }
